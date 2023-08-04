4d7e2fc95a4e94cc436d98d3d33f22c8126a139d
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-08-04 19:10:14 -0400

    wrote storage retrieval loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,19 @@ const formEl = $('#search-form');
 const buttonEl = $('#search-button');
 const historyEl = $('#search-history');
 
-function printHistory() {
-    historyEl.append(`<button type="button" class="btn btn-secondary w-100 mb-3">${formEl.val()}</button>`)
-}
-
 
 $(document).ready(function () {
     
     // Save search entries to history
     storageKey = 0;
     buttonEl.on('click', function() {
-        historyEl.append(`<button type="button" class="btn btn-secondary w-100 mb-3">${formEl.val()}</button>`);
-        localStorage.setItem(storageKey, formEl.val());
+        historyEl.prepend(`<button type="button" class="btn btn-secondary w-100 mb-3">${formEl.val()}</button>`);
+        localStorage.setItem(storageKey, `<button type="button" class="btn btn-secondary w-100 mb-3">${formEl.val()}</button>`);
         storageKey++;
     });
-
+    // Get history from storage
+    for (i=0; i<localStorage.length; i++) {
+        storageItem = localStorage.getItem(i);
+        historyEl.prepend(storageItem);
+    }
 });
