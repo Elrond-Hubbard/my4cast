d345f7d8dead37608640662dcfd7c44d7b36df6a
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-08-09 19:50:53 -0400

    comments & code cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const currentEl = $('#current');
 
 function fetchForecast(city) {
 
-    // Fetch coordinates for given city
+    // Coordinates are fetched for a given city.
     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=8a1eef5ee7f9bf0ec404673fdde28868`)
         .then(response => {
             return response.json();
@@ -17,7 +17,7 @@ function fetchForecast(city) {
             lat = (response[0].lat);
             lon = (response[0].lon);
 
-            // Fetch current weather for given coordinates
+            // Current weather is called for given coordinates.
             fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=8a1eef5ee7f9bf0ec404673fdde28868&units=imperial`)
                 .then(response => {
                     return response.json();
@@ -25,7 +25,7 @@ function fetchForecast(city) {
                 .then(response => {
                     currentEl.empty();
 
-                    // Build card for current weather
+                    // A card containing current weather data is appended to the page.
                     currentEl.append(`
                         <div class="card-body">
                             <h5 class="card-title">${response.name} today</h5>
@@ -36,7 +36,7 @@ function fetchForecast(city) {
                         </div>`)
                 })
 
-            // Fetch 5 day forecast for given coordinates
+            // A 5 day forecast is called for given coordinates
             fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=8a1eef5ee7f9bf0ec404673fdde28868&units=imperial`)
                 .then(response => {
                     return response.json();
@@ -44,7 +44,7 @@ function fetchForecast(city) {
                 .then(response => {
                     forecastEl.empty();
                     console.log(response)
-                    // For each day in the forecast, build a card
+                    // For each day in the forecast, a card containing forecast data is appended to the page.
                     for (var i = 6; i < response.list.length; i += 8) {
                         var time = (response.list[i].dt_txt);
                         console.log(response.list[i].dt_txt)
@@ -67,24 +67,24 @@ function fetchForecast(city) {
 $(document).ready(function () {
 
 
-    // Get history from storage
+    // Search history is retrieved from local storage and displayed on the page.
     for (i = 0; i < localStorage.length; i++) {
         storageItem = localStorage.getItem(i);
         historyEl.prepend(storageItem);
     }
 
-    // Save search entries to history
+    // When the search button is pressed, input is stored locally and passed to the main forecast function.
     storageKey = (localStorage.length);
     buttonEl.on('click', function () {
         historyEl.prepend(`<button id="history-button" type="button" class="btn btn-secondary w-100 mb-3">${formEl.val()}</button>`);
         localStorage.setItem(storageKey, `<button id="history-button" type="button" class="btn btn-secondary w-100 mb-3">${formEl.val()}</button>`);
         storageKey++;
 
-        // Return forecast for search input
+        // Search input is used to call weather data
         fetchForecast(formEl.val());
     });
 
-    // Return forecast for history buttons
+    // Alternatively, weather data is called by clicking a search button.
     historyEl.on('click', 'button', function() {
         fetchForecast($(this).text())
     })
